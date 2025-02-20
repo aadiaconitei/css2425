07fccbf11c413c09f00e2ba8ad7fb895e17de660
Profesor C1 <admin@PROF-C1.local>
2025-02-20 17:15:23 +0200

    curs5

--- curs4/Aplicatii_curs4/js/app2.js
+++ curs4/Aplicatii_curs4/js/app2.js
@@ -1,11 +1,11 @@
 //aplicatia ap3
 function showResponsive() {
     var elements = document.getElementsByClassName("main");
+    var boxes = document.getElementsByClassName("box");
     Array.prototype.forEach.call(elements, function (el) {
-        var boxes = document.getElementsByClassName("box");
+        
         if (el.style.flexDirection === "column") {
             el.style.flexDirection = "row";
-            
             Array.prototype.forEach.call(boxes, function (box) {
                 box.style.width = '300px';
             });
